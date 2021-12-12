337d1922077f34f6d819aaf3a0dd7d4d98f86124
LeoPRhon <leonardo.proano@est.ikiam.edu.ec>
2021-12-11 22:05:14 -0500

    Modificación 6

--- netsize.sh
+++ netsize.sh
@@ -4,7 +4,7 @@
 #En primer lugar, se selecciona la ubicacion de la carpeta "Saavedra2013" en el dispositivo
 #En segundo lugar, en el interior de la carpeta se crea un documento en txt.
 cd Saavedra2013
-# Tercero, Finalmente aplicamos el codigo, y en lo personal agrege una leyenda para diferencias el numero de columnas de las filas 
+# En tercer lugar, Finalmente aplicamos el codigo, y en lo personal agrege una leyenda para diferencias el numero de columnas de las filas 
 echo "El ńumero de columnas es:" ;
 head -n1 n1.txt | grep -o " " | wc -l ;
 echo "El numero de filas es:" ;
